e7ac09d2db60bbca4c8ca7d310fa90846ea6ceb1
GreyDev <greyc9404@gmail.com>
2025-08-29 17:41:07 -0400

    build.sh v2

--- build.sh
+++ build.sh
@@ -1,9 +1,23 @@
+# Editar el archivo para agregar la primera línea
+echo '#!/usr/bin/env bash
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Convert static asset files
 python manage.py collectstatic --no-input
 
-python manage.py migrate
+python manage.py migrate' > build.sh
+
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@test.com', 'admin123')
+    print('Superusuario creado: admin/admin123')
+else:
+    print('Superusuario ya existe')
+"
+
+# Commit los cambios
+git add build.sh
+git commit -m "Add shebang to build.sh"
+git push
