6d73613b8b20758e769d691df1a0fe3e7b9a93d8
GreyDev <greyc9404@gmail.com>
2025-09-09 16:51:23 -0300

    final8

--- apps/usuarios/serializers.py
+++ apps/usuarios/serializers.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 from .models import Usuario
-from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.hashers import make_password
 
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = '__all__'
+        fields = [
+            'id', 'email', 'nombre', 'apellido', 'rol',
+            'activo', 'fecha_creacion', 'fecha_modificacion'
+        ]
         read_only_fields = ('fecha_creacion', 'fecha_modificacion')
-        extra_kwargs = {
-            'password': {'write_only': True}  # no sale en respuestas
-        }
 
 class UsuarioCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -22,10 +22,6 @@ class UsuarioCreateSerializer(serializers.ModelSerializer):
         validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
 
-class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    password = serializers.CharField()
-
 class UsuarioUpdateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False, allow_blank=True)
 
@@ -34,14 +30,13 @@ class UsuarioUpdateSerializer(serializers.ModelSerializer):
         fields = ['email', 'password', 'nombre', 'apellido', 'rol', 'activo']
 
     def update(self, instance, validated_data):
-
         password = validated_data.pop('password', None)
-        
         if password:
             instance.password = make_password(password)
-            
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
-  
         return super().update(instance, validated_data)
-    
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
