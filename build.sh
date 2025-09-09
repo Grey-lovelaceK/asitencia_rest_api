ef79ea537cd069a8696a00084bc7d3f352abef77
GreyDev <greyc9404@gmail.com>
2025-09-09 18:12:35 -0300

    final18

--- build.sh
+++ build.sh
@@ -9,20 +9,28 @@ python manage.py migrate
 python manage.py shell -c "
 from apps.usuarios.models import Usuario
 
-usuario, created = Usuario.objects.get_or_create(
-    email='greyc9404@gmail.com'
-)
+email = 'greyc9404@gmail.com'
+password = 'admin123'
+
+usuario, created = Usuario.objects.get_or_create(email=email)
 
 if created:
-    Usuario.objects.create_superuser(
-        email='greyc9404@gmail.com',
-        password='admin123',
+    # Si no existía, crear superusuario
+    Usuario.objects.filter(email=email).update(
         nombre='Administrador',
-        apellido='Principal'
+        apellido='Principal',
+        rol='administrador',
+        is_staff=True,
+        is_superuser=True
     )
+    usuario.set_password(password)
+    usuario.save()
     print('✅ Usuario administrador creado')
 else:
-    usuario.set_password('admin123')
+    # Si existía, actualizar contraseña
+    usuario.set_password(password)
+    usuario.is_staff = True
+    usuario.is_superuser = True
     usuario.save()
     print('🔄 Usuario administrador existente actualizado con nueva contraseña')
 "
