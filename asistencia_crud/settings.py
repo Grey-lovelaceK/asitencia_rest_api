44defbe3a8edea39e8a5caa663aba6e648188c1d
GreyDev <greyc9404@gmail.com>
2025-09-10 02:12:57 -0300

    agregar cors de produccion front end

--- asistencia_crud/settings.py
+++ asistencia_crud/settings.py
@@ -187,6 +187,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000", 
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "https://front-asistencia.vercel.app",
 ]
 
 # Con JWT ya no necesitas cookies de sesión para la API
